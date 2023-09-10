6ad3764991afb94d1d030326c994f1941f559174
Jacob Rivera <jakemichaelrivera32@gmail.com>
2023-09-09 19:50:33 -0500

    Add imageToASCII.py and development

---
+++ spotify-player/application.py
@@ -0,0 +1,38 @@
+import tkinter
+import customtkinter
+
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x150")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
+        self.button.pack(padx=20, pady=20)
+
+        self.label = customtkinter.CTkLabel(self, text="THIS IS MY LABEL", fg_color="transparent")
+        self.label.pack(padx=20, pady=20)
+    
+    def button_callbck(self):
+        print("button clicked")
+
+
+
+def startApp():
+
+    # System Settings
+    customtkinter.set_appearance_mode("System")
+    customtkinter.set_default_color_theme("blue")
+
+    # Our app frame
+    app = customtkinter.CTk()
+    app.geometry("720x480")
+    app.title("Spotify Player")
+
+    # Adding UI Elements
+    title = customtkinter.CTkLabel(app, text="what song")
+    title.pack(padx=10, pady=10)
+
+    # Run app
+    app.mainloop()
--- spotify-player/main.py
+++ spotify-player/main.py
@@ -1,17 +1,3 @@
-'''
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import cred
-
-
-scope = "user-read-recently-played"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.CLIENT_ID, client_secret= cred.CLIENT_SECRET, redirect_uri=cred.REDIRECT_URL, scope=scope))
-
-# auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
-# sp = spotipy.Spotify(auth_manager=auth_manager)
-'''
-
 import os
 import sys
 import json
@@ -20,6 +6,11 @@ import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 import requests
+import application
+
+RUN_APP = 1
+
+DB = 1
 
 
 def setUpSpotify():
@@ -41,15 +32,28 @@ def setUpSpotify():
 
 def getCurrentDeviceInfo(spotifyObj):
     devices = spotifyObj.devices()
-    print(json.dumps(devices, sort_keys=True, indent=4))
+    if DB: print(json.dumps(devices, sort_keys=True, indent=4))
     deviceID = devices['devices'][0]['id']
     return devices['devices'][0]
 
+def getCurrentlyPlaying(spotifyObj):
+    track = spotifyObj.current_user_playing_track()
+    if DB: print(json.dumps(track, sort_keys=True, indent=4))    # Could get a lot more info!
+    if track:
+        artist = track['item']['artists'][0]['name']
+        trackName = track['item']['name']
+        print("Currently playing " + artist + " - " + trackName)
+    else:
+        print("Nothing is currently playing")
 
 def main():
     spotifyObj = setUpSpotify()
     getCurrentDeviceInfo(spotifyObj=spotifyObj)
+    getCurrentlyPlaying(spotifyObj)
 
+    if (RUN_APP):
+        app = application.App()
+        app.mainloop()
 
 
 if __name__ == "__main__":
